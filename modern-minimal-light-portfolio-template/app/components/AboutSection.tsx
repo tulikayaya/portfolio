3e3f8af9e62d92a88ee731256fe1e8fa5685499b
tulikayaya <87579199+tulikayaya@users.noreply.github.com>
2025-09-01 13:59:14 -0400

    Update AboutSection.tsx

--- modern-minimal-light-portfolio-template/app/components/AboutSection.tsx
+++ modern-minimal-light-portfolio-template/app/components/AboutSection.tsx
@@ -56,7 +56,7 @@ export default function AboutSection() {
 								cursor
 								cursorStyle="|"
 								loop={1}
-								onTypeDone={() => setTypedDone(true)}
+								onLoopDone={() => setTypedDone(true)}
 							/>
 						) : (
 							''
